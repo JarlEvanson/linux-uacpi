94df7f1efe95a72e8d7f924a0dab4ebd3b5c7e51
Jarl Evanson <evanson.jarl@gmail.com>
2026-07-09 17:50:18 -0400

    acpi: fill out `shim_handler.c`

--- drivers/acpi/uacpi/shim_handler.c
+++ drivers/acpi/uacpi/shim_handler.c
@@ -2,34 +2,34 @@
 
 acpi_status acpi_install_sci_handler(acpi_sci_handler address, void *context)
 {
-	BUG();
+	return AE_SUPPORT;
 }
 
 acpi_status acpi_remove_sci_handler(acpi_sci_handler address)
 {
-	BUG();
+	return AE_SUPPORT;
 }
 
 acpi_status acpi_install_global_event_handler(acpi_gbl_event_handler handler,
 					      void *context)
 {
-	BUG();
+	return AE_SUPPORT;
 }
 
 acpi_status acpi_install_notify_handler(acpi_handle device, u32 handler_type,
 					acpi_notify_handler handler,
 					void *context)
 {
-	BUG();
+	return AE_SUPPORT;
 }
 
 acpi_status acpi_remove_notify_handler(acpi_handle device, u32 handler_type,
 				       acpi_notify_handler handler)
 {
-	BUG();
+	return AE_SUPPORT;
 }
 
 acpi_status acpi_install_exception_handler(acpi_exception_handler handler)
 {
-	BUG();
+	return AE_SUPPORT;
 }
