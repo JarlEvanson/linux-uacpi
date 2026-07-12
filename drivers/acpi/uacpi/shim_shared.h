67c46e6fb9d03bd378ea84cc3d7b69f13ce621cc
Jarl Evanson <evanson.jarl@gmail.com>
2026-07-12 07:59:10 -0400

    uACPI shim: implement `shim_subsystem.c`

--- drivers/acpi/uacpi/shim_shared.h
+++ drivers/acpi/uacpi/shim_shared.h
@@ -1,7 +1,10 @@
 #pragma once
 
+#include <linux/acpi.h>
+
 #include <uacpi/kernel_api.h>
 #include <uacpi/log.h>
+#include <uacpi/status.h>
 
 /* logging interface and implementation adapted from uacpi/internal/log.h */
 bool shim_should_log(uacpi_log_level level);
@@ -21,3 +24,54 @@ bool shim_should_log(uacpi_log_level level);
 #define shim_info(...) shim_log(UACPI_LOG_INFO, __VA_ARGS__)
 #define shim_warn(...) shim_log(UACPI_LOG_WARN, __VA_ARGS__)
 #define shim_error(...) shim_log(UACPI_LOG_ERROR, __VA_ARGS__)
+
+static inline acpi_status uacpi_to_acpi_status(uacpi_status status)
+{
+	switch (status) {
+	case UACPI_STATUS_OK:
+		return AE_OK;
+	case UACPI_STATUS_MAPPING_FAILED:
+		return AE_NO_MEMORY;
+	case UACPI_STATUS_OUT_OF_MEMORY:
+		return AE_NO_MEMORY;
+	case UACPI_STATUS_BAD_CHECKSUM:
+		return AE_BAD_CHECKSUM;
+	case UACPI_STATUS_INVALID_SIGNATURE:
+		return AE_BAD_SIGNATURE;
+	case UACPI_STATUS_INVALID_TABLE_LENGTH:
+		return AE_BAD_HEADER;
+	case UACPI_STATUS_NOT_FOUND:
+		return AE_NOT_FOUND;
+	case UACPI_STATUS_INVALID_ARGUMENT:
+		return AE_BAD_PARAMETER;
+	case UACPI_STATUS_UNIMPLEMENTED:
+		return AE_NOT_IMPLEMENTED;
+	case UACPI_STATUS_ALREADY_EXISTS:
+		return AE_ALREADY_EXISTS;
+	case UACPI_STATUS_INTERNAL_ERROR:
+		return AE_ERROR;
+	case UACPI_STATUS_TYPE_MISMATCH:
+		return AE_TYPE;
+	case UACPI_STATUS_INIT_LEVEL_MISMATCH:
+		return AE_BAD_PARAMETER;
+	case UACPI_STATUS_NAMESPACE_NODE_DANGLING:
+		return AE_NULL_ENTRY;
+	case UACPI_STATUS_NO_HANDLER:
+		return AE_NOT_EXIST;
+	case UACPI_STATUS_NO_RESOURCE_END_TAG:
+		return AE_AML_NO_RESOURCE_END_TAG;
+	case UACPI_STATUS_COMPILED_OUT:
+		return AE_SUPPORT;
+	case UACPI_STATUS_HARDWARE_TIMEOUT:
+	case UACPI_STATUS_TIMEOUT:
+		return AE_TIME;
+	case UACPI_STATUS_OVERRIDDEN:
+		return AE_OK;
+	case UACPI_STATUS_DENIED:
+		return AE_ACCESS;
+	default:
+		if (status >= UACPI_STATUS_AML_UNDEFINED_REFERENCE)
+			return AE_AML_INTERNAL;
+		return AE_ERROR;
+	}
+}
--- drivers/acpi/uacpi/shim_subsystem.c
+++ drivers/acpi/uacpi/shim_subsystem.c
@@ -1,31 +1,88 @@
 #include <linux/acpi.h>
 
+#include <uacpi/uacpi.h>
+
+#include "shim_shared.h"
+
 acpi_status ACPI_INIT_FUNCTION acpi_initialize_subsystem(void)
 {
-	BUG();
+	acpi_status acpi_st;
+	uacpi_status st;
+	uacpi_u64 flags;
+
+	acpi_current_gpe_count = 0;
+
+	acpi_st = acpi_os_initialize();
+	if (acpi_st != AE_OK)
+		return acpi_st;
+
+	if (!acpi_gbl_create_osi_method)
+		flags |= UACPI_FLAG_NO_OSI;
+	if (acpi_gbl_enable_table_validation)
+		flags |= UACPI_FLAG_PROACTIVE_TBL_CSUM;
+	if (acpi_gbl_do_not_use_xsdt)
+		flags |= UACPI_FLAG_BAD_XSDT;
+
+	flags |= UACPI_FLAG_NO_ACPI_MODE;
+	st = uacpi_initialize(flags);
+	if (st != UACPI_STATUS_OK)
+		return uacpi_to_acpi_status(st);
+
+	return AE_OK;
 }
 
 acpi_status ACPI_INIT_FUNCTION acpi_enable_subsystem(u32 flags)
 {
-	BUG();
+	uacpi_status st;
+
+	// TODO: handle additional flags.
+	if (!(flags & ACPI_NO_ACPI_ENABLE)) {
+		st = uacpi_enter_acpi_mode();
+		if (st != UACPI_STATUS_OK)
+			return uacpi_to_acpi_status(st);
+	}
+
+	return AE_OK;
 }
 
 acpi_status ACPI_INIT_FUNCTION acpi_load_tables(void)
 {
-	BUG();
+	uacpi_status st;
+
+	st = uacpi_namespace_load();
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status ACPI_INIT_FUNCTION acpi_initialize_objects(u32 flags)
 {
-	BUG();
+	uacpi_status st;
+
+	// TODO: handle additional flags.
+	if (flags != ACPI_FULL_INITIALIZATION) {
+		shim_error("acpi_initialize_objects(%x) is not supported",
+			   flags);
+		return AE_SUPPORT;
+	}
+
+	st = uacpi_namespace_initialize();
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_subsystem_status(void)
 {
-	BUG();
+	uacpi_init_level level;
+
+	level = uacpi_get_current_init_level();
+	switch (level) {
+	case UACPI_INIT_LEVEL_NAMESPACE_INITIALIZED:
+		return AE_OK;
+	default:
+		return AE_ERROR;
+	}
 }
 
 acpi_status ACPI_INIT_FUNCTION acpi_terminate(void)
 {
-	BUG();
+	uacpi_state_reset();
+	return AE_OK;
 }
