c51f6a75ce7661a5d5d78ffa8a61daea052fcb49
Jarl Evanson <evanson.jarl@gmail.com>
2026-07-12 09:42:44 -0400

    uACPI shim: implement `shim_output.c`

--- drivers/acpi/uacpi/shim_output.c
+++ drivers/acpi/uacpi/shim_output.c
@@ -1,43 +1,116 @@
 #include <linux/acpi.h>
 
+#include "shim_shared.h"
+
 void ACPI_INTERNAL_VAR_XFACE acpi_info(const char *format, ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	shim_info("%s", buffer);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_warning(const char *module_name,
 					  u32 line_number, const char *format,
 					  ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	shim_warn("%s:%u: %s", module_name, line_number, buffer);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_error(const char *module_name,
 					u32 line_number, const char *format,
 					...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	shim_error("%s:%u: %s", module_name, line_number, buffer);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_exception(const char *module_name,
 					    u32 line_number, acpi_status status,
 					    const char *format, ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+	const char *formatted_status;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	formatted_status = acpi_format_exception(status);
+	shim_error("%s:%u: %s (%s)", module_name, line_number, buffer,
+		   formatted_status);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_bios_warning(const char *module_name,
 					       u32 line_number,
 					       const char *format, ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	shim_warn("%s:%u: ACPI Firmware Warning: %s", module_name, line_number,
+		  buffer);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_bios_error(const char *module_name,
 					     u32 line_number,
 					     const char *format, ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	shim_error("%s:%u: ACPI Firmware Error: %s", module_name, line_number,
+		   buffer);
 }
 
 void ACPI_INTERNAL_VAR_XFACE acpi_bios_exception(const char *module_name,
@@ -45,5 +118,19 @@ void ACPI_INTERNAL_VAR_XFACE acpi_bios_exception(const char *module_name,
 						 acpi_status status,
 						 const char *format, ...)
 {
-	BUG();
+	va_list args;
+	char buffer[512];
+	int printed;
+	const char *formatted_status;
+
+	va_start(args, format);
+	printed = vsnprintf(buffer, sizeof(buffer), format, args);
+	va_end(args);
+
+	if (printed <= 0)
+		return;
+
+	formatted_status = acpi_format_exception(status);
+	shim_error("%s:%u: ACPI Firmware Error: %s (%s)", module_name,
+		   line_number, buffer, formatted_status);
 }
