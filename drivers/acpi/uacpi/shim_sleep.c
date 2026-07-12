5919dd94f26ef66dfea79b6c935c32fd8d42ff27
Jarl Evanson <evanson.jarl@gmail.com>
2026-07-12 08:08:55 -0400

    uACPI shim: implement `shim_sleep.c`

--- drivers/acpi/uacpi/shim_sleep.c
+++ drivers/acpi/uacpi/shim_sleep.c
@@ -1,25 +1,90 @@
 #include <linux/acpi.h>
 
+#include <uacpi/sleep.h>
+
+#include "shim_shared.h"
+
+static acpi_status acpi_to_uacpi_sleep_state(u8 sleep_state,
+					     uacpi_sleep_state *out)
+{
+	switch (sleep_state) {
+	case ACPI_STATE_S0:
+		*out = UACPI_SLEEP_STATE_S0;
+		break;
+	case ACPI_STATE_S1:
+		*out = UACPI_SLEEP_STATE_S1;
+		break;
+	case ACPI_STATE_S2:
+		*out = UACPI_SLEEP_STATE_S2;
+		break;
+	case ACPI_STATE_S3:
+		*out = UACPI_SLEEP_STATE_S3;
+		break;
+	case ACPI_STATE_S4:
+		*out = UACPI_SLEEP_STATE_S4;
+		break;
+	case ACPI_STATE_S5:
+		*out = UACPI_SLEEP_STATE_S5;
+		break;
+	default:
+		acpi_os_printf("uACPI does not recognize sleep state %x",
+			       sleep_state);
+		return AE_NOT_IMPLEMENTED;
+	}
+
+	return AE_OK;
+}
+
 acpi_status
 acpi_set_firmware_waking_vector(acpi_physical_address physical_address,
 				acpi_physical_address physical_address64)
 {
-	BUG();
+	uacpi_status st;
+
+	st = uacpi_set_waking_vector(physical_address, physical_address64);
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_enter_sleep_state_prep(u8 sleep_state)
 {
-	BUG();
+	uacpi_sleep_state state;
+	acpi_status acpi_st;
+	uacpi_status st;
+
+	acpi_st = acpi_to_uacpi_sleep_state(sleep_state, &state);
+	if (ACPI_FAILURE(acpi_st))
+		return acpi_st;
+
+	st = uacpi_prepare_for_sleep_state(state);
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_leave_sleep_state_prep(u8 sleep_state)
 {
-	BUG();
+	uacpi_sleep_state state;
+	acpi_status acpi_st;
+	uacpi_status st;
+
+	acpi_st = acpi_to_uacpi_sleep_state(sleep_state, &state);
+	if (ACPI_FAILURE(acpi_st))
+		return acpi_st;
+
+	st = uacpi_prepare_for_wake_from_sleep_state(state);
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_enter_sleep_state(u8 sleep_state)
 {
-	BUG();
+	uacpi_sleep_state state;
+	acpi_status acpi_st;
+	uacpi_status st;
+
+	acpi_st = acpi_to_uacpi_sleep_state(sleep_state, &state);
+	if (ACPI_FAILURE(acpi_st))
+		return acpi_st;
+
+	st = uacpi_enter_sleep_state(state);
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_enter_sleep_state_s4bios(void)
@@ -29,11 +94,90 @@ acpi_status acpi_enter_sleep_state_s4bios(void)
 
 acpi_status acpi_leave_sleep_state(u8 sleep_state)
 {
-	BUG();
+	uacpi_sleep_state state;
+	acpi_status acpi_st;
+	uacpi_status st;
+
+	acpi_st = acpi_to_uacpi_sleep_state(sleep_state, &state);
+	if (ACPI_FAILURE(acpi_st))
+		return acpi_st;
+
+	st = uacpi_wake_from_sleep_state(state);
+	return uacpi_to_acpi_status(st);
 }
 
 acpi_status acpi_get_sleep_type_data(u8 sleep_state, u8 *sleep_type_a,
 				     u8 *sleep_type_b)
 {
-	BUG();
+	uacpi_sleep_state state;
+	uacpi_object *sleep_package, *package_object;
+	uacpi_object_array sleep_package_contents;
+	uacpi_u64 val;
+	acpi_status acpi_st;
+	uacpi_status st;
+	char path[] = "_Sx";
+
+	if (!sleep_type_a || !sleep_type_b)
+		return AE_BAD_PARAMETER;
+
+	acpi_st = acpi_to_uacpi_sleep_state(sleep_state, &state);
+	if (ACPI_FAILURE(acpi_st))
+		return acpi_st;
+
+	path[2] = '0' + (char)state;
+	st = uacpi_eval_simple_package(uacpi_namespace_root(), path,
+				       &sleep_package);
+	if (st != UACPI_STATUS_OK)
+		return uacpi_to_acpi_status(st);
+
+	st = uacpi_object_get_package(sleep_package, &sleep_package_contents);
+	if (st != UACPI_STATUS_OK) {
+		uacpi_object_unref(sleep_package);
+		return uacpi_to_acpi_status(st);
+	}
+
+	switch (sleep_package_contents.count) {
+	case 0:
+		uacpi_object_unref(sleep_package);
+		return AE_AML_PACKAGE_LIMIT;
+	case 1:
+		package_object = sleep_package_contents.objects[0];
+		if (!uacpi_object_is(package_object, UACPI_OBJECT_INTEGER)) {
+			uacpi_object_unref(sleep_package);
+			return AE_AML_OPERAND_TYPE;
+		}
+
+		st = uacpi_object_get_integer(package_object, &val);
+		if (st != UACPI_STATUS_OK) {
+			uacpi_object_unref(sleep_package);
+			return uacpi_to_acpi_status(st);
+		}
+
+		*sleep_type_a = (u8)val;
+		*sleep_type_b = (u8)(val >> 8);
+		uacpi_object_unref(sleep_package);
+		return AE_OK;
+	default:
+		package_object = sleep_package_contents.objects[0];
+
+		st = uacpi_object_get_integer(package_object, &val);
+		if (st != UACPI_STATUS_OK) {
+			uacpi_object_unref(sleep_package);
+			return uacpi_to_acpi_status(st);
+		}
+
+		*sleep_type_a = (u8)val;
+
+		package_object = sleep_package_contents.objects[1];
+
+		st = uacpi_object_get_integer(package_object, &val);
+		if (st != UACPI_STATUS_OK) {
+			uacpi_object_unref(sleep_package);
+			return uacpi_to_acpi_status(st);
+		}
+
+		*sleep_type_b = (u8)val;
+		uacpi_object_unref(sleep_package);
+		return AE_OK;
+	}
 }
